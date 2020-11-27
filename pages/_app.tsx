53b52264a8f4e956d994f352ff02f7b8241cc530
Musashi Sakamoto <1292602b@gmail.com>
2020-11-26 22:17:32 -0800

    material-ui introduce

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
-import type { AppProps } from 'next/app';
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+import { AppProps } from 'next/app';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client';
 
@@ -8,10 +13,31 @@ const client = new ApolloClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (      
+    <React.Fragment>
+    <Head>
+      <title>My page</title>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+    </Head>
+    <ThemeProvider theme={theme}>
     <ApolloProvider client={client}>
+
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
       <Component {...pageProps} />
     </ApolloProvider>
+
+    </ThemeProvider>
+  </React.Fragment>
   );
 }
 
